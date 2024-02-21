53c816343d1d04167da82a000733a56862b4b37c
MIN <alsrb4171@gmail.com>
2024-02-21 14:11:27 +0900

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 9384 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const [_, ...input] = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+let answer = [];
+input.forEach((v, i) => {
+  const [a, b] = v.split(' ').map(Number);
+  answer.push(`Case #${i + 1}: ${a + b}`);
+})
+console.log(answer.join('\n'));
