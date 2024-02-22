49a6657dc558f7b8a26670b2427052c05bb7f2b4
MIN <alsrb4171@gmail.com>
2024-02-22 10:14:53 +0900

    [Bronze II] Title: 상수, Time: 116 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString().trim().split(' ');
+const num1 = +input[0].split('').reverse().join('');
+const num2 = +input[1].split('').reverse().join('');
+console.log(Math.max(num1, num2));
