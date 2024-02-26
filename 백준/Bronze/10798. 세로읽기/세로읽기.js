27d3ee58379d81e4262a11eb1911ae6bbf1c6b45
MIN <alsrb4171@gmail.com>
2024-02-26 13:49:20 +0900

    [Bronze I] Title: 세로읽기, Time: 140 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+const max = Math.max(...input.map((v) => v.length));
+let answer = '';
+for (let i = 0; i < max; i++) {
+  input.forEach((v) => {
+    answer += v.trim()[i] || '';
+  });
+}
+console.log(answer);
