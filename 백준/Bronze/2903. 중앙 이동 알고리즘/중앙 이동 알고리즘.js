ef1208300d7ec92edbf26f2e1dbb9762f0dc95b3
MIN <alsrb4171@gmail.com>
2024-04-05 22:11:06 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 124 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.js
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.js
@@ -1,10 +1,4 @@
 const fs = require('fs');
 let n = fs.readFileSync('./dev/stdin').toString().trim();
 
-let answer = 2;
-
-for (let i = 0; i < n; i++) {
-  answer += 2 ** i;
-}
-
-console.log(answer ** 2);
+console.log(4 ** n + 2 ** (+n + 1) + 1);
