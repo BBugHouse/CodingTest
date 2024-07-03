73cd1d62ff4ca2330e5d59aff0fbc81006ea6c74
버그 하우스 <alsrb4171@gmail.com>
2024-07-03 12:06:59 +0900

    [Bronze III] Title: 세 막대, Time: 128 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('./dev/stdin')
+  .toString()
+  .trim()
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const sum = input[0] + input[1];
+
+console.log(sum + (sum <= input[2] ? sum - 1 : input[2]));
